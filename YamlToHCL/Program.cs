57377afe96ed7834ce757d91db79f2b0b1c791bc
Jonathan Giddings <jonathan.giddings@aspeninsights.com>
2022-10-17 14:37:39 -0500

    Add support for Terraform stuff

--- YamlToHCL/Program.cs
+++ YamlToHCL/Program.cs
@@ -7,14 +7,17 @@ Dictionary<Type, int> _TypeOrder = new()
     { typeof(Dictionary<object, object>), 1}
 };
 
-var fileName = "";
-var _sort = Sort.None;
+var fileName = String.Empty;
+var sort = Sort.None;
 var verbose = false;
+var terraform = Terraform.none;
+
+const char _indentChar = ' ';
+const int _indentFactor = 3;
 
 var index = 0;
 foreach(var arg in args)
 {
-    index++;
     if (arg.StartsWith("-"))
         switch (arg)
         {
@@ -23,14 +26,20 @@ foreach(var arg in args)
                 break;
 
             case "--sort" or "-s":
-                if (!Enum.TryParse<Sort>(args[index + 1], out _sort))
+                if (!Enum.TryParse<Sort>(args[index + 1], out sort))
                     Console.WriteLine($"Invalid Sort. Sort must be one of {Enum.GetValues(typeof(Sort)).Cast<Sort>().Aggregate("", (current, next) => current + ", " + next)}");
                 break;
 
             case "--verbose" or "-v":
                 verbose = true;
                 break;
+
+            case "--terraform" or "-t":
+                if (!Enum.TryParse<Terraform>(args[index + 1], out terraform))
+                    Console.WriteLine($"Invalid Terraform. Sort must be one of {Enum.GetValues(typeof(Terraform)).Cast<Sort>().Aggregate("", (current, next) => current + ", " + next)}");
+                break;
         }
+    index++;
 }
 
 var yaml = new List<string>() { String.Empty };
@@ -58,9 +67,38 @@ var deserializer = new DeserializerBuilder().Build();
 
 try
 {
-    yaml.Select(x => deserializer.Deserialize<Dictionary<object, object>>(x))
-        .ToList()
-        .ForEach(x => NestedDictIteration(x));
+    var dicts = yaml.Select(x => deserializer.Deserialize<Dictionary<object, object>>(x));
+    
+    foreach (var dict in dicts)
+    {
+        switch (terraform)
+        {
+            case Terraform.none:
+                NestedDictIteration(dict);
+                break;
+
+            case Terraform.kubernetes_manifest:
+                var resource = new Dictionary<object, object>()
+                {
+                    { "manifest", dict }
+                };
+
+                Console.WriteLine("resource \"kubernetes_manifest\" {");
+                NestedDictIteration(resource, 0);
+                Console.WriteLine("}");
+                Console.WriteLine();
+                break;
+
+            case Terraform.kubectl_manifest:
+                Console.WriteLine("resource \"kubectl_manifest\" {");
+                Console.WriteLine($"{getPrefix(_indentChar, 1, _indentFactor)}yaml_body = yamlencode(");
+                NestedDictIteration(dict, 1);
+                Console.WriteLine($"{getPrefix(_indentChar, 1, _indentFactor)})");
+                Console.WriteLine("}");
+                Console.WriteLine();
+                break;
+        }
+    }
 }
 catch (Exception)
 {
@@ -72,9 +110,6 @@ catch (Exception)
 
 void NestedDictIteration(Dictionary<object, object> nestedDict, int indent = -1)
 {
-    const char _indentChar = ' ';
-    const int _indentFactor = 3;
-
     nestedDict = sortDict(nestedDict);
 
     indent++;
@@ -85,50 +120,50 @@ void NestedDictIteration(Dictionary<object, object> nestedDict, int indent = -1)
         if (kvp.Value is string value)
         {
             if (String.IsNullOrEmpty(value))
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = \"\"");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = \"\"");
             else if (value.All(Char.IsDigit) || value == "true" || value == "false") //Do not include quotes for numerical or boolean values (YamlDotNet interprets all numerical values as strings, so this is the best we have)
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = {value}");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = {value}");
             else
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = \"{value}\"");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = \"{value}\"");
 
             continue;
         }
         else if (kvp.Value is List<object> list)
         {
             if (list.Count == 0)
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = []");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = []");
             else if (list.Count == 1 && list[0] is string singleItem)
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = [ \"{singleItem}\" ]"); //Single string lists don't need to be on multiple lines
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = [ \"{singleItem}\" ]"); //Single string lists don't need to be on multiple lines
             else
             {
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = [");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = [");
                 indent++;
 
                 foreach (var item in list)
                     if (item is string str)
-                        Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}\"{str}\"");
+                        Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}\"{str}\"");
                     else
                     {
-                        Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{{");
+                        Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{{");
                         NestedDictIteration((Dictionary<object, object>)item, indent);
-                        Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}}}");
+                        Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}}}");
                     }
 
                 indent--;
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}]");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}]");
             }
         }
         else if (kvp.Value is Dictionary<object, object> dictionary)
         {
             if (dictionary.Count > 0)
             {
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = {{");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = {{");
                 NestedDictIteration(dictionary, indent);
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}}}");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}}}");
                 if (indent == 0) Console.WriteLine(); //Add an extra new line at the end of top level dictionary entries
             }
             else
-                Console.WriteLine($"{getIndent(_indentChar, indent, _indentFactor)}{key} = {{}}");
+                Console.WriteLine($"{getPrefix(_indentChar, indent, _indentFactor)}{key} = {{}}");
         }
         else
             NestedDictIteration((Dictionary<object, object>)kvp.Value, indent);
@@ -136,10 +171,10 @@ void NestedDictIteration(Dictionary<object, object> nestedDict, int indent = -1)
     indent--;
 }
 
-static string getIndent(char indentChar, int indentCount, int indentFactor) => new(indentChar, indentCount * indentFactor);
+static string getPrefix(char indentChar, int indentCount, int indentFactor) => new(indentChar, indentCount * indentFactor);
 
 Dictionary<object, object> sortDict(Dictionary<object, object> dict) =>
-    _sort switch
+    sort switch
     {
         Sort.Alphabetical => dict.Select(x => x)
                                  .OrderBy(x => x.Key)
@@ -159,3 +194,10 @@ enum Sort
     Type,
     None
 };
+
+enum Terraform
+{
+    kubernetes_manifest,
+    kubectl_manifest,
+    none
+}
